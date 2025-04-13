154d5d0a01d79b3ce394a2690c0f59262b1bea0f
Maxim <maxon7591@mail.ru>
2025-04-13 20:59:25 +1000

    Задание «Маршруты в Flask»

--- 1234.py
+++ 1234.py
@@ -3,25 +3,23 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-
 def get_greeting():
-    current_hour = datetime.now().hour
-
-    if 6 <= current_hour < 12:
+    now = datetime.now()
+    hour = now.hour
+    if 6 <= hour < 12:
         return "Доброе утро"
-    elif 12 <= current_hour < 18:
+    elif 12 <= hour < 18:
         return "Добрый день"
-    elif 18 <= current_hour < 24:
+    elif 18 <= hour < 24:
         return "Добрый вечер"
     else:
         return "Доброй ночи"
-
 
 @app.route('/')
 def index():
     greeting = get_greeting()
     return render_template('index.html', greeting=greeting)
 
-
 if __name__ == '__main__':
     app.run(debug=True)
+
